d7b124f6467cd76a77d222bf688d275012c968c0
PrzemekHJN <przemyslaw.hojny@gmail.com>
2024-04-27 14:04:56 +0200

    pierwsze zmiany

--- ExpenseManager/Program.cs
+++ ExpenseManager/Program.cs
@@ -1,3 +1,6 @@
+using ExpenseManager.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace ExpenseManager
 {
     public class Program
@@ -8,6 +11,9 @@ namespace ExpenseManager
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
             var app = builder.Build();
 
